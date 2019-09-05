3e09c30524e734de44c1500f2510bd203d8e2341
nram1234 <redanaser0@gmail.com>
2019-09-05 08:35:37 +0200

    o

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      supportedLocales: [Locale('en', 'US'), Locale('ar')],
+      supportedLocales: [Locale('en'), Locale('ar')],
       localizationsDelegates: [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
